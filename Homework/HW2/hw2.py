169209ae9f6241362dd685e14298339e76685a0d
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-02-05 22:52:29 -0700

    Update hw2.py

---
+++ Homework/HW2/hw2.py
@@ -0,0 +1,38 @@
+# APPM 4600 - HW#2
+# Author: Becca Blum
+#***********************************
+import numpy as np
+import matplotlib.pyplot as plt 
+
+## Question 3
+
+
+## Question 4
+
+# a)
+t = np.arange(0,np.pi,np.pi/30)
+Y = np.cos(t)
+
+S = np.sum(np.dot(t,Y))
+
+print("the sum is: " + str(S))
+
+# b)
+R = 1.2
+dr = 0.1
+f = 15
+p = 0
+theta = np.linspace(0,2*np.pi,100)
+
+x = lambda theta,R,dr,f,p: R*(1+dr*np.sin(f*theta+p))*np.cos(theta)
+y = lambda theta,R,dr,f,p: R*(1+dr*np.sin(f*theta+p))*np.sin(theta)
+
+w = x(theta,R,dr,f,p)
+v = y(theta,R,dr,f,p)
+
+plt.plot(theta,w)
+plt.plot(theta,v)
+
+plt.show()
+
+
