77793bd270e9a57b7dde0645253a8e1cb653aa14
BeccaBlum <rebl2759@colorado.edu>
2024-02-12 23:41:16 -0700

    Updated Pre-Lab 4 code

--- Homework/HW2/hw2.py
+++ Homework/HW2/hw2.py
@@ -3,16 +3,13 @@
 #***********************************
 import numpy as np
 import matplotlib.pyplot as plt 
-
-## Question 3
-
+import math
 
 ## Question 4
 
 # a)
 t = np.arange(0,np.pi,np.pi/30)
 Y = np.cos(t)
-
 S = np.sum(np.dot(t,Y))
 
 print("the sum is: " + str(S))
@@ -30,9 +27,22 @@ y = lambda theta,R,dr,f,p: R*(1+dr*np.sin(f*theta+p))*np.sin(theta)
 w = x(theta,R,dr,f,p)
 v = y(theta,R,dr,f,p)
 
-plt.plot(theta,w)
-plt.plot(theta,v)
+#plt.plot(theta,w)
+#plt.plot(theta,v)
 
-plt.show()
+#plt.show()
+
+R = 1.2
+dr = 0.05
+f = 2
+p = 1.2
+
+for i in range(10):
+    w = x(theta,i,dr,2+i,p)
+    v = y(theta,i,dr,2+i,p)
+    plt.plot(theta,w)
+    plt.plot(theta,v)
+
+plt.show
 
 
--- Labs/Lab_4/lab_4.py
+++ Labs/Lab_4/lab_4.py
@@ -7,50 +7,34 @@ def fixedpt(f,x0,tol,Nmax):
     ''' x0 = initial guess''' 
     ''' Nmax = max number of iterations'''
     ''' tol = stopping tolerance'''
-
+    x_list = [x0]
     count = 0
     while (count <Nmax):
        count = count +1
        x1 = f(x0)
+       x_list.append(x1)
        if (abs(x1-x0) <tol):
           xstar = x1
           ier = 0
-          return [xstar,ier]
+          x_array = np.array(x_list)
+          return [xstar, ier, x_array]
        x0 = x1
 
     xstar = x1
     ier = 1
-    return [xstar, ier]
+    x_array = np.array(x_list)
+    return [xstar, ier, x_array]
     
 
-# use routines 
-#f1 = lambda x: 1+0.5*np.sin(x)
-#f1 = lambda x: x*(1 + (7-x**5)/x**2)**3
-#f1 = lambda x: x - (x**5-7)/x**2
-f1 = lambda x: x - (x**5-7)/(5*x**4)
-#f1 = lambda x: x - (x**5-7)/(12)
-''' 
-fixed point is alpha1 = 1.4987....
-'''
-
-f2 = lambda x: 3+2*np.sin(x)
-''' 
-fixed point is alpha2 = 3.09... 
-'''
+f1 =lambda x: (10/(x+4))**(1/2)
 
 Nmax = 100
-tol = 1e-10
+tol = 10e-10
 
 ''' test f1 '''
-x0 = 1#7**(1/5)
-[xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+x0 = 1.5
+[xstar,ier, x_list] = fixedpt(f1,x0,tol,Nmax)
 print('the approximate fixed point is:',xstar)
 print('f1(xstar):',f1(xstar))
 print('Error message reads:',ier)
-    
-''' test f2 '''
-x0 = 0.0
-[xstar,ier] = fixedpt(f2,x0,tol,Nmax)
-print('the approximate fixed point is:',xstar)
-print('f2(xstar):',f2(xstar))
-print('Error message reads:',ier)
+print(len(x_list))
