675e0a0697bc8c9c571df4def5a8d1c73eb770fd
BeccaBlum <rebl2759@colorado.edu>
2024-02-16 00:20:14 -0700

    Updated HW 4 code

--- Homework/HW3/fixedpt_example.py
+++ Homework/HW3/fixedpt_example.py
@@ -21,36 +21,16 @@ def fixedpt(f,x0,tol,Nmax):
     xstar = x1
     ier = 1
     return [xstar, ier]
-    
 
 # use routines 
 #f1 = lambda x: 1+0.5*np.sin(x)
-#f1 = lambda x: x*(1 + (7-x**5)/x**2)**3
-#f1 = lambda x: x - (x**5-7)/x**2
-f1 = lambda x: x - (x**5-7)/(5*x**4)
-#f1 = lambda x: x - (x**5-7)/(12)
-''' 
-fixed point is alpha1 = 1.4987....
-'''
 
-f2 = lambda x: 3+2*np.sin(x)
-''' 
-fixed point is alpha2 = 3.09... 
-'''
+#Nmax = 100
+#tol = 1e-10
 
-Nmax = 100
-tol = 1e-10
-
-''' test f1 '''
-x0 = 1#7**(1/5)
-[xstar,ier] = fixedpt(f1,x0,tol,Nmax)
-print('the approximate fixed point is:',xstar)
-print('f1(xstar):',f1(xstar))
-print('Error message reads:',ier)
-    
-''' test f2 '''
-x0 = 0.0
-[xstar,ier] = fixedpt(f2,x0,tol,Nmax)
-print('the approximate fixed point is:',xstar)
-print('f2(xstar):',f2(xstar))
-print('Error message reads:',ier)
+#''' test f1 '''
+#x0 = 1#7**(1/5)
+#[xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+#print('the approximate fixed point is:',xstar)
+#print('f1(xstar):',f1(xstar))
+#print('Error message reads:',ier)
--- Homework/HW3/hw3.py
+++ Homework/HW3/hw3.py
@@ -4,12 +4,31 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import math
+from fixedpt_example import fixedpt
+from bisection_example import bisection
 
 # Question 1
-
+def Q1():
+    f1 = lambda x : 2*x - 1 - np.sin(x)
+    
+    tol = 0.5*10**(-8)
 
 # Question 2
-
+def Q2():
+    a = 4.82
+    b = 5.2
+    tol = 1*10**(-4)
+    Nmax = 100
+# a) 
+    f2a = lambda x : (x - 5)**9
+    [astar_a,ier_a] = bisection(f2a,a,b,tol,Nmax)
+    print("root is: " + str(astar_a) + "\n")
+    print("error message: " + str(ier_a) + "\n")
+# b)
+    f2b = lambda x : x**9-45*x**8+900*x**7-10500*x**6+78750*x**5-393750*x**4+1312500*x**3-2812500*x**2+3515625*x-1953125
+    [astar_b,ier_b] = bisection(f2b,a,b,tol,Nmax)
+    print("root is: " + str(astar_b) + "\n")
+    print("error message: " + str(ier_b) + "\n")
 
 # Question 3
 
@@ -28,11 +47,28 @@ def Q4():
 
 # Question 5
 def Q5():
-    f5 = lambda x : x - 4*np.sin(2*x) - 3
+    f5 = lambda x : x/4 - np.sin(2*x) - 3/4
+    x0_list = [-0.9, -0.57, 1.6, 3.2, 4.5]
+    tol = 0.5*10**(-10)
+    Nmax = 100
+    x_star_list = []
+    for x0 in x0_list:
+        [xstar,ier] = fixedpt(f5,x0,tol,Nmax)
+        x_star_list.append(xstar)
+        print(ier)
+    print(x_star_list)
 
+    x = np.linspace(-2,8,100)
+    y = f5(x)
+    plt.plot(x,y)
+    plt.plot(x,y*0)
+    plt.legend(["f(x)", "x=0, for reference"])
+    plt.xlabel("x"), plt.ylabel("f(x)")
+    plt.title("Plot of f(x) = x - 4sin(2x) -3")
+    plt.show()
 
 def main():
-    Q4()
+    Q2()
 
 if __name__ == "__main__":
     main()
