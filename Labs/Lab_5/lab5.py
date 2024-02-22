c6ecd50df44479d6a1360f67030c97cbd0b647ad
BeccaBlum <rebl2759@colorado.edu>
2024-02-21 22:50:08 -0700

    Updated Lab 5 code (from yesterday)

--- Labs/Lab_5/lab5.py
+++ Labs/Lab_5/lab5.py
@@ -23,28 +23,33 @@ def pre_bisection(f,dfdx, d2fdx2, a,b,tol,Nmax):
     if (fa*fb>0):
        ier = 1
        astar = a
-       return [astar, ier]
+       return [astar, ier, count]
 
     ''' verify end point is not a root '''
     if (fa == 0):
       astar = a
       ier =0
-      return [astar, ier]
+      return [astar, ier, count]
 
     if (fb ==0):
       astar = b
       ier = 0
-      return [astar, ier]
+      return [astar, ier, count]
 
     count = 0
     while (count < Nmax):
       c = 0.5*(a+b)
       fc = f(c)
 
+      if abs((f(c)*d2fdx2(c))/(dfdx(c))**2) < 1:
+        astar = c
+        ier = "Newton"
+        return [astar, ier, count]
+
       if (fc ==0):
         astar = c
         ier = 0
-        return [astar, ier]
+        return [astar, ier, count]
 
       if (fa*fc<0):
          b = c
@@ -54,30 +59,34 @@ def pre_bisection(f,dfdx, d2fdx2, a,b,tol,Nmax):
       else:
         astar = c
         ier = 3
-        return [astar, ier]
+        return [astar, ier, count]
 
       if (abs(b-a)<tol):
         astar = a
         ier =0
-        return [astar, ier]
+        return [astar, ier, count]
       
       count = count +1
 
     astar = a
     ier = 2
-    return [astar,ier] 
+    return [astar,ier, count] 
 
 # use routines    
-f = lambda x: np.sin(x)
-dfdx = lambda x: 
-d2fdx2
+f = lambda x: np.e**(x**2 + 7*x - 30) - 1
+dfdx = lambda x: (2*x + 7)*np.e**(x**2 + 7*x - 30)
+d2fdx2 = lambda x: (2*x + 7)*(2*x + 7)*np.e**(x**2 + 7*x - 30) + (2)*np.e**(x**2 + 7*x - 30)
 
-a = 0.5 #-1#0.5 #1
-b = 3*np.pi/4 #2 #0.5#2 #4
+a = 2
+b = 4.5
 
 Nmax = 100
 tol = 1e-5
 
-[astar,ier] = bisection(f,a,b,tol,Nmax)
+[astar,ier,count] = pre_bisection(f,dfdx, d2fdx2,a,b,tol,Nmax)
 print('the approximate root is',astar)
 print('the error message reads:',ier)
+print('number of iterations is', count)
+
+# Newton’s method with x0 = 4.5.
+
