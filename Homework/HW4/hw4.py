7e938edbcffce5bbc085342bf13862884d25c733
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-03 08:47:32 -0700

    Create hw4.py

---
+++ Homework/HW4/hw4.py
@@ -0,0 +1,18 @@
+# APPM 4600 - Homework 4
+# Author: Becca Blum
+# Date: 3/3/24
+#*****************************************************************************
+from import 
+import numpy as np
+
+# Question 1 
+def q1():
+  f = lambda x,y: 3*x**2 - y**2
+  g = lambda x,y: 3*x*y**2 - x**3 - 1
+
+  
+# a)
+  while (() > TOL) and n >= Nmax:
+    x_y = np.array([x_n, y_n]) - np.matmul(np.array([[1./6., 1./18.],[0, 1./6.]]), np.array([f(x_n, y_n), g(x_n, y_n)))
+    
+  
