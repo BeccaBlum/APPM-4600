5282f9a833b69c82dcf17a3ecdc60de01d3cdbd8
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-03 08:52:47 -0700

    Update prelab6.py

--- Labs/Lab_6/prelab6.py
+++ Labs/Lab_6/prelab6.py
@@ -17,12 +17,27 @@ h = 0.01*2.**(-np.arange(0,10))
 x = np.pi/2
 
 for_f_prime = forward_dif(f,h,x)
+for_e = np.abs(dfdx(x) - for_f_prime)
 
 cen_f_prime = centered_dif(f,h,x)
+cen_e = np.abs(dfdx(x) - cen_f_prime)
 
-plt.plot(h, for_f_prime)
-plt.plot(h, for_f_prime)
-plt.plot(h, [0]*len(h) + dfdx(x))
-plt.legend(["forward difference", "centered difference", "f'(pi/2) = -sin(pi/2)"])
-plt.title("Approximating f'(x) using Forward and Centered Differences \n f(x) = cos(x), x = pi/2")
+plt.plot(h, np.log10(for_e))
+plt.plot(h, np.log10(cen_e))
+plt.legend(["forward difference", "centered difference"])
+plt.title("Error from approximating f'(x) using Forward and Centered Differences \n f(x) = cos(x), x = pi/2")
+plt.ylabel("log_10(e)")
+plt.xlabel("h")
+plt.show()
+
+print(for_f_prime)
+print(cen_f_prime)
+
+
+plt.plot(-h, np.log10(for_e))
+plt.plot(-h, np.log10(cen_e))
+stuff = 100000000000000*(-h)**5
+print(h)
+print(stuff)
+plt.plot(-h, stuff)
 plt.show()
