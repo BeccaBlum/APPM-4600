bff612444c6b94e8156c9924f7f2d046db1cebd2
BeccaBlum <rebl2759@colorado.edu>
2024-03-11 23:12:53 -0600

    Updated Prelab 8

--- Labs/Lab_8/make_line.py
+++ Labs/Lab_8/make_line.py
@@ -1,4 +1,5 @@
 def make_line(x, y, alpha):
   # Makes a line through two input points and evalulates it at point alpha
-  f_alpha = ((y[0]-y[1])/(x[0]-x[1]))*alpha
+  f_alpha = ((y[0]-y[1])/(x[0]-x[1]))*(alpha - x[0]) + y[0]
+
   return f_alpha
