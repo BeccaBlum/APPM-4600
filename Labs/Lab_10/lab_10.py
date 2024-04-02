55fde082623f74d414bc0e2dfb5b2e7cdf540585
BeccaBlum <rebl2759@colorado.edu>
2024-04-02 09:57:09 -0600

    Updated Lab 10

--- Labs/Lab_10/lab_10.py
+++ Labs/Lab_10/lab_10.py
@@ -1,1 +1,13 @@
+def eval_legendre(n, x):
+    phi = [1, x]
+    for i in range(1,n):
+        phi.append(1/(i+1)*((2*i + 1)*x*phi[i] - i*phi[i-1]))
 
+    return phi
+
+def chebychev(n,x):
+    T = [1, x]
+    for i in range(1,n):
+        T.append(2*x*T[i] - T[i-1])
+
+    return T
