6d64f507850953ab2833df0e62aee4a9890b5cf0
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-04-09 08:03:40 -0600

    Update prelab.py

--- Labs/Lab_11/prelab.py
+++ Labs/Lab_11/prelab.py
@@ -1,1 +1,12 @@
 # Prelab 
+
+def trapezoid(a, b, f, N):
+    h = (b-a)/N
+    
+    for i in range(1,N-1):
+        middle = f(a+i*h)
+        
+    middle_sum = sum(middle)
+    F = (h/2)*(f(a) + 2*middle_sum + f(b))
+    
+    return F
