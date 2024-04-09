81bbcd285d196691e4c63ac9dd9a73d9449884f3
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-04-09 08:05:14 -0600

    Update prelab.py

--- Labs/Lab_11/prelab.py
+++ Labs/Lab_11/prelab.py
@@ -1,4 +1,5 @@
 # Prelab 
+# int_a^bf(x)dx aprox= h/n(f(a) + 2 sum_{i=1}^{n-1}f(a-ih)+f(b)
 
 def trapezoid(a, b, f, N):
     h = (b-a)/N
@@ -7,6 +8,6 @@ def trapezoid(a, b, f, N):
         middle = f(a+i*h)
         
     middle_sum = sum(middle)
-    F = (h/2)*(f(a) + 2*middle_sum + f(b))
+    F = (h/2)*(f(a) + 2*middle_sum + f(b)) # integral approx
     
     return F
