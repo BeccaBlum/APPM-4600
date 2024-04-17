692f6adae9f64324a7e1e54052850a292a1c911c
BeccaBlum <rebl2759@colorado.edu>
2024-04-17 11:33:15 -0600

    Updated Project

--- Final_Project/ridge_estimator.py
+++ Final_Project/ridge_estimator.py
@@ -1,6 +1,77 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+def ridge_regres(A,b,gamma, deg):
+    s = len(A)
+    leftside = np.matmul(np.transpose(A),A) + gamma*np.identity(deg+1)
+    rightside = np.matmul(np.transpose(A),b)
+    x_star = np.linalg.solve(leftside,rightside)
 
+    return x_star
+
+n = 20
+k = 10
+N = 100
+deg = 1
 x = np.linspace(0,10,20)
-y = 3*x + 2
+f = 3*x + 2
 
+gamma = np.arange(-2,4,0.1)
+
+RSS_min = np.zeros(N)
+RSS_min_index = np.zeros(N)
+gammas = np.zeros(N)
+RSS_list = []
+
+for ii in range(N):
+    noise = np.random.normal(0,1,n)
+    y = 3*x + 2 + .2*noise
+
+    j = 0
+    y_points = np.zeros(k); x_points = np.zeros(k)
+    x_control = np.zeros(k); y_control = np.zeros(k)
+    for i in range(n):
+        if (i % 2) == 0:
+            y_points[j] = y[i]
+            x_points[j] = x[i]
+        else:
+            x_control[j] = x[i]
+            y_control[j] = y[i]
+            j = j+1
+
+    A = np.transpose([x_points**0, x_points**1])
+
+    RSS = []
+    for i in range(len(gamma)):
+        x_star = ridge_regres(A,y_points,gamma[i], deg)
+        P = x_star[1]*x + x_star[0]
+        P_control = x_star[1]*x_control + x_star[0]
+
+        Err_squ = abs(P_control - y_control)**2
+
+        RSS.append(np.sum(Err_squ)) #Residual sum of squares
+    print(ii)
+    #print(RSS)
+    RSS_min_index[ii] = int(np.where(RSS == np.min(RSS))[0][0])
+    RSS_min[ii] = int(np.min(RSS))
+    print(RSS_min[ii])
+    gammas[ii] = gamma[int(RSS_min_index[ii])]
+    RSS_list = [RSS_list, np.array(RSS)]
+
+#print(RSS_list[0][0][0][0][0])
+#print(RSS_min)
+#print(gammas)
+plt.scatter(gammas, RSS_min)
+plt.show()
+
+#for 
+plt.plot(range(len(gamma)), RSS_list)
+plt.show()
+
+'''
+plt.plot(x,P)
+plt.scatter(x_points,y_points)
+plt.scatter(x_control,y_control)
+plt.plot(x,f)
+plt.show()
+'''
